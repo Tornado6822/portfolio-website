95d2f47bc6d65f862f900f86c6afc78353bd76ed
Tornado6822 <morelpatrick97@gmail.com>
2026-05-24 08:24:26 -0400

    Images + wrapper styling for CaseStudies

--- portfolio-website/src/components/CaseStudies.jsx
+++ portfolio-website/src/components/CaseStudies.jsx
@@ -1,32 +1,34 @@
+import "./CaseStudies.css";
+
 function CaseStudies() {
   return (
     <section className="d-flex justify-content-center">
       <div className="container">
         <div className="row align-items-center">
           <div className="col-6 col-md-3 d-flex justify-content-center">
-            <div className="img-wrapper bg-primary">
+            <div className="img-wrapper">
               <img
                 src="./bikeRepairService.webp"
                 alt="Bike Repair Service"
-                className="img-fluid"
+                className=""
               />
             </div>
           </div>
           <div className="col-6 col-md-3 d-flex justify-content-center">
-            <div className="img-wrapper bg-primary">
+            <div className="img-wrapper">
               <img
                 src="./memoryGameThumbnail.webp"
                 alt="Memory game"
-                className="img-fluid"
+                className=""
               />
             </div>
           </div>
           <div className="col-6 col-md-3 d-flex justify-content-center">
-            <div className="img-wrapper bg-primary">
+            <div className="img-wrapper">
               <img
                 src="eCommerceThumbnail.webp"
                 alt="E-commerce"
-                className="img-fluid"
+                className=""
               />
             </div>
           </div>
@@ -35,7 +37,7 @@ function CaseStudies() {
               <img
                 src="visualizationThumbnail.webp"
                 alt="Analytics"
-                className="img-fluid"
+                className=""
               />
             </div>
           </div>
