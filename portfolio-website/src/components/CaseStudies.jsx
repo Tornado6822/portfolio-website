5b58307622fddf553aa7142b909f09e43f459c05
Tornado6822 <morelpatrick97@gmail.com>
2026-05-24 13:18:40 -0400

    Finished case study cards

--- portfolio-website/src/components/CaseStudies.jsx
+++ portfolio-website/src/components/CaseStudies.jsx
@@ -4,41 +4,66 @@ function CaseStudies() {
   return (
     <section className="d-flex justify-content-center">
       <div className="container">
-        <div className="row align-items-center">
-          <div className="col-6 col-md-3 d-flex justify-content-center">
-            <div className="img-wrapper">
-              <img
-                src="./bikeRepairService.webp"
-                alt="Bike Repair Service"
-                className=""
-              />
+        <h1 className="pt-5">Case Studies</h1>
+        <div className="row align-items-center mt-3">
+          <div className="col-6 col-md-3 d-flex justify-content-center card">
+            <div className="card">
+              <div className="img-wrapper">
+                <img
+                  src="./bikeRepairService.webp"
+                  alt="Bike Repair Service"
+                  className=""
+                />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">Bike Repair</h5>
+                <p>Service Site Design</p>
+              </div>
             </div>
           </div>
           <div className="col-6 col-md-3 d-flex justify-content-center">
-            <div className="img-wrapper">
-              <img
-                src="./memoryGameThumbnail.webp"
-                alt="Memory game"
-                className=""
-              />
+            <div className="card">
+              <div className="img-wrapper card">
+                <img
+                  src="./memoryGameThumbnail.webp"
+                  alt="Memory game"
+                  className="card-img-top"
+                />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">Memory Game</h5>
+                <p>UI Interaction</p>
+              </div>
             </div>
           </div>
           <div className="col-6 col-md-3 d-flex justify-content-center">
-            <div className="img-wrapper">
-              <img
-                src="eCommerceThumbnail.webp"
-                alt="E-commerce"
-                className=""
-              />
+            <div className="card">
+              <div className="img-wrapper">
+                <img
+                  src="eCommerceThumbnail.webp"
+                  alt="E-commerce"
+                  className=""
+                />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">E-Commerce Site</h5>
+                <p>Placeholder</p>
+              </div>
             </div>
           </div>
           <div className="col-6 col-md-3 d-flex justify-content-center">
-            <div className="img-wrapper">
-              <img
-                src="visualizationThumbnail.webp"
-                alt="Analytics"
-                className=""
-              />
+            <div className="card">
+              <div className="img-wrapper">
+                <img
+                  src="visualizationThumbnail.webp"
+                  alt="Analytics"
+                  className=""
+                />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">Analytics</h5>
+                <p>Placeholder</p>
+              </div>
             </div>
           </div>
         </div>
